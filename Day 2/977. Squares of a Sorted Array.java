c25b1974fcd8ce854249cedba3efa6f3b0a37c3e
yakimax <sv1099912@gmail.com>
2023-07-18 12:15:59 +0530

    day2

---
+++ Day 2/977. Squares of a Sorted Array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int i = 0 ;
+        int j = nums.length - 1 ; 
+        int k = j ;
+        int arr[] = new int[j+1] ;
+        while( i <= j ){
+            if( Math.abs(nums[i]) > Math.abs(nums[j]) ){
+                arr[k--] = nums[i] * nums[i] ;
+                i++ ;
+            }else{
+                arr[k--] = nums[j] * nums[j] ;
+                j-- ;
+            }
+        }
+        return arr ;
+    }
+}
