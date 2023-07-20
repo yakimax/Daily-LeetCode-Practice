b16197039b1ad445f9a06437e015e4afea330803
yakimax <sv1099912@gmail.com>
2023-07-20 15:27:17 +0530

    add

---
+++ Day 4/229. Majority Element II.java
@@ -0,0 +1,42 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        
+        List<Integer> list = new ArrayList<>() ;
+        int freq = nums.length / 3 ;
+        int first = 0,second = 0,count1 = 0 ,count2 = 0 ;
+        
+        for( int i = 0 ; i < nums.length ; i++ ){
+            if(nums[i] == first){
+                count1++ ;
+            }else if(nums[i] == second){
+                count2++ ;
+            }else if(count1 == 0){
+                first = nums[i] ;
+                count1 = 1 ;
+            }else if(count2 == 0){
+                second = nums[i]  ;
+                count2 = 1 ;
+            }else{
+                count1--;
+                count2--;
+            }
+        }
+        count1 = 0 ;
+        count2 = 0 ;
+        int n = nums.length / 3  ;
+        for( int i = 0 ; i < nums.length ; i++ ){
+            if(nums[i]== first){
+                count1++ ;
+            }else if(nums[i]== second){
+                count2++ ;
+            }
+        }
+        if(count1 > n){
+                list.add(first);
+            }
+            if(count2 > n){
+                list.add(second);
+            }
+        return list ;
+    }
+}
