ee1cde583657d0cb41035caaf0652fe41373ca0c
yakimax <sv1099912@gmail.com>
2023-07-22 13:49:46 +0530

    add

--- Day 5/795. Number of Subarrays with Bounded Maximum.java
+++ Day 5/795. Number of Subarrays with Bounded Maximum.java
@@ -1,6 +1,16 @@
-package Day 5;
-
-
-public class 795. Number of Subarrays with Bounded Maximum {
-    
+class Solution {
+    public int numSubarrayBoundedMax(int[] nums, int left, int right) {
+        int s = -1; 
+        int e = -1 ;
+        int ans = 0 ; 
+        for(int i = 0 ; i < nums.length ; i++){
+            if( nums[i] >= left && nums[i] <= right ){
+                e = i ;
+            }else if(nums[i]>right){
+                s=e=i ;
+            }
+            ans+= e-s ;
+        }
+        return ans ;
+    }
 }
