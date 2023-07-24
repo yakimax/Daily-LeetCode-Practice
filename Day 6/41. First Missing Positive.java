a0fabf396a721b7416acda0942fbeb09385632e3
yakimax <sv1099912@gmail.com>
2023-07-24 16:38:45 +0530

    add

--- Day 6/41. First Missing Positive.java
+++ Day 6/41. First Missing Positive.java
@@ -1,5 +1,30 @@
-package Day 6;
+class Solution {
+    public int firstMissingPositive( int[] nums ) {
+        int n = nums.length  ; 
+        
+        for( int i = 0 ; i < n; i++ ){
+            if( nums[i] > n  || nums[i] <= 0 ){
+                nums[i]= n + 1 ;
+            }
+        }
+        
+        for( int i = 0 ; i < n ; i++ ){
+            int num = Math.abs(nums[i]) ;
+            if(num>n){
+                continue ;
+            }
+            num-- ;
+            if(nums[num] > 0){ 
+                nums[num] = -1*nums[num] ;
+            }
+        }
+        
+        for( int i = 0 ; i < n ; i++ ){
+            if( nums[i] >= 0 ){
+                return i + 1 ;
+            }
+        }
 
-public abstract class 41. First Missing Positive {
-    
+        return n+1 ;
+    }
 }
