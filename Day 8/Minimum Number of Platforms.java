4e22796d930799382a2974ed3f6bcffd865f9c56
yakimax <sv1099912@gmail.com>
2023-07-27 23:59:37 +0530

    add

--- Day 8/Minimum Number of Platforms.java
+++ Day 8/Minimum Number of Platforms.java
@@ -2,5 +2,24 @@ package Day 8;
 
 public class MinimumNumberofPlatforms{
     
-    
+    Arrays.sort(dt) ;
+        Arrays.sort(at) ;
+        int count = 1 ;
+        int i = 1 ;
+        int j = 0 ;
+        int stations = 1 ;
+        while( i < dt.length ){
+            if( at[i] <= dt[j] ){
+                stations++ ;
+                i++ ;
+                 
+            }else{
+                stations-- ;
+                j++ ;
+            }
+            if( stations > count ){
+                count = stations ;
+            }
+        }
+        return count ;
 }
