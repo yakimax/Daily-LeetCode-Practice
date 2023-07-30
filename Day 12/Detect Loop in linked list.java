60c54878dcc351cebe672b2d7a12d9f24641c309
yakimax <sv1099912@gmail.com>
2023-07-30 22:15:15 +0530

    add

---
+++ Day 12/Detect Loop in linked list.java
@@ -0,0 +1,30 @@
+
+class Solution1 {
+    public static boolean detectLoop(Node head){                         //using hashSet of node address
+        HashSet<Node>hs = new HashSet<>() ;
+        while(head != null){
+            hs.add(head);
+            head = head.next ;
+            if(hs.contains(head)){
+                return true ;
+            }
+        }
+        return false ;
+    }
+}
+
+class Solution {
+    public static boolean detectLoop(Node head){                           //using rabiit and turle floyed algorithms
+        Node Rabbit = head ;
+        Node turtle = head ;
+        
+        while( Rabbit != null && Rabbit.next != null ){
+            Rabbit = Rabbit.next.next ;
+            turtle = turtle.next ;
+            if( Rabbit == turtle ){
+                return true ;
+            }
+        }
+        
+        return false ;
+    }
