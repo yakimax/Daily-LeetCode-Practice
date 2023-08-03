9d38ee0f0b0d9e5d82776e062dcc731bd5fd5d5e
yakimax <sv1099912@gmail.com>
2023-08-03 22:27:36 +0530

    add

---
+++ Day 16/codingBat3.java
@@ -0,0 +1,17 @@
+public boolean groupSumClump(int start, int[] nums, int target) {
+  if(target == 0){
+    return true ;
+  }
+  if(start == nums.length){
+    return false ;
+  }
+  int i = 1 ;
+  while(start < nums.length-1 && nums[start]==nums[start+1]){
+    i++ ;
+    start++ ;
+  }
+  boolean a = groupSumClump(start+1,nums,target-(nums[start]*i)) ;
+  boolean b = groupSumClump(start+1,nums,target) ;
+  
+  return a||b ;
+}
