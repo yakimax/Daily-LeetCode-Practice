761944090b1ccc9fee5eea952183e1af7b81520b
yakimax <sv1099912@gmail.com>
2023-08-03 19:19:08 +0530

    add

---
+++ Day 16/codingBat2.java
@@ -0,0 +1,19 @@
+public boolean groupSum6(int start, int[] nums, int target) {
+  if(nums.length == start){
+      if( target == 0 ){
+        return true ; 
+      }else{
+        return false ;
+      }
+  }
+  boolean a ;
+  if(nums[start]==6){
+    target-=6 ;
+    a = groupSum6(start+1,nums,target) ;
+  }else{
+    a = groupSum6(start+1,nums,target-nums[start]) ;
+  }
+  boolean b = groupSum6(start+1,nums,target) ;
+  
+  return a||b ;
+}
