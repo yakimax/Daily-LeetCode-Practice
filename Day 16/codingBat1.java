f7c2fc5e37799247580d992078bcbefb67daefe6
yakimax <sv1099912@gmail.com>
2023-08-03 19:18:29 +0530

    add

---
+++ Day 16/codingBat1.java
@@ -0,0 +1,11 @@
+public boolean groupNoAdj(int start, int[] nums, int target) {
+  if(target == 0){
+    return true ;
+  }
+  if(nums.length <= start){
+    return false ;
+  }
+    boolean a = groupNoAdj(start+2,nums,target-nums[start]) ;
+    boolean b = groupNoAdj(start+1 ,nums,target) ;
+    return a||b ;
+}
