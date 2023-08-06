07a717ecf1acf38b4ae8bc79eefc27e21f0d3ad5
yakimax <sv1099912@gmail.com>
2023-08-06 15:03:03 +0530

    add

---
+++ Day 17/CodingBat.java
@@ -0,0 +1,14 @@
+public boolean splitArray(int[] nums) {
+    return helper(nums,0,0,0) ;
+  }
+  
+  public boolean helper(int []nums ,int i , int sum1 , int  sum2 ){
+    if(i==nums.length){
+      return sum1 == sum2 ? true : false ;
+    }
+    
+    boolean a = helper(nums,i+1,sum1+nums[i],sum2) ;
+    boolean b = helper(nums,i+1,sum1,sum2+nums[i]) ; 
+    
+    return a||b ;
+  }
